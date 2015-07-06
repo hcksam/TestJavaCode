84cae5ca6c9d9f8564339e452016393a2278501a
01311588 <hcksam@hotmail.com>
2015-07-06 18:30:08 +0800

    test String replace

---
+++ TestJavaCode/src/String/TestReplace.java
@@ -0,0 +1,11 @@
+package String;
+
+public class TestReplace {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "abc #test def";
+		System.out.println(s.replaceAll("#test", "sam"));
+	}
+
+}
