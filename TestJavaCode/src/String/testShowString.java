7b922683eb53dc2f43370e7ada4f311e851079c3
hck <hcksam@hotmail.com>
2015-07-31 16:37:51 +0800

    testShowString

---
+++ TestJavaCode/src/String/testShowString.java
@@ -0,0 +1,17 @@
+package String;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class testShowString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] as = {"1","2","3"};
+		ArrayList<String> als = new ArrayList<String>();
+		als.add("a");
+		als.add("b");
+		als.add("c");
+	}
+
+}
