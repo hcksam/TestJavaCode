28a132ebd71cbb254be1e96cd63f3b224d2a5f9d
hcksam <hcksam@hotmail.com>
2016-03-16 16:21:01 +0800

    test Chinese

--- TestJavaCode/src/String/TestChinese.java
+++ TestJavaCode/src/String/TestChinese.java
@@ -11,9 +11,18 @@ public class TestChinese {
 		// TODO Auto-generated method stub
 		String s = "中文eng!@#";
 		System.out.println(s);
-		System.out.println(UnicodeBlock.of(s.charAt(0)));
-		System.out.println(UnicodeBlock.of(s.charAt(2)));
-		System.out.println(UnicodeBlock.of(s.charAt(5)));
+		System.out.println(isWritten(s.charAt(0)));
+		System.out.println(isWritten(s.charAt(2)));
+		System.out.println(isWritten(s.charAt(5)));
 	}
+	
+	public static boolean isWritten(Character inChar){
+        boolean isWritten = false;
+        int codePoint = inChar.hashCode();
 
+        if (Character.UnicodeBlock.of(codePoint) == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS || Character.isAlphabetic(codePoint)) {
+        	isWritten = true;
+        }
+        return isWritten;
+    }
 }
