8ab1eb82a65650c77291edf21c036431ede710d9
hcksam <hcksam@hotmail.com>
2016-05-11 18:07:01 +0800

    test double

---
+++ TestJavaCode/src/Double/TestDouble.java
@@ -0,0 +1,12 @@
+package Double;
+
+public class TestDouble {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Double d = Double.valueOf(String.valueOf(""));
+		Double d = null;
+		System.out.println(d);
+	}
+
+}
