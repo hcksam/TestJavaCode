3cb29a00f44ca07e90971cc59dd2309ca64b06eb
hcksam <hcksam@hotmail.com>
2016-06-22 18:06:17 +0800

    BigDecimal

---
+++ TestJavaCode/src/decimal/TestDecimal.java
@@ -0,0 +1,13 @@
+package decimal;
+
+import java.math.BigDecimal;
+
+public class TestDecimal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BigDecimal d = new BigDecimal("9.999999999999");
+		System.out.println(d);
+	}
+
+}
