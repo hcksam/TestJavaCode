8b0f093843d04efddaab21ddedc89107c9a5ae4e
hcksam <hcksam@hotmail.com>
2016-10-27 15:43:16 +0800

    split

--- TestJavaCode/src/String/TestSpilt.java
+++ TestJavaCode/src/String/TestSpilt.java
@@ -1,5 +1,7 @@
 package String;
 
+import java.util.regex.Pattern;
+
 public class TestSpilt {
 
 	/**
@@ -7,8 +9,8 @@ public class TestSpilt {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String s = "a&//*b";
-		String[] ss = s.split("[&/*]");
+		String s = "a||b||0";
+		String[] ss = s.split(Pattern.quote("||"));
 		for (int i=0;i<ss.length;i++){
 			System.out.println(ss[i]);
 		}
