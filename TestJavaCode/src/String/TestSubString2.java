187a9285c7f2bed5825a2d789fb19dc5f07087ec
hck <hcksam@hotmail.com>
2017-02-21 14:19:22 +0800

    update

--- TestJavaCode/src/String/TestSubString2.java
+++ TestJavaCode/src/String/TestSubString2.java
@@ -11,10 +11,19 @@ public class TestSubString2 {
 		String bs2 = s.substring(0, 6);
 		String es1 = s.substring(6,6+5);
 		String es2 = s.substring(6+5);
+		String ss1 = s.substring(6);
 		System.out.println(bs1);
 		System.out.println(bs2);
 		System.out.println(es1);
 		System.out.println(es2);
+		System.out.println(ss1);
+		
+//		String s = "12345_11";
+//		int index = s.lastIndexOf("_");
+//		String ss1 = s.substring(0, index);
+//		String ss2 = s.substring(index+1);
+//		System.out.println(ss1);
+//		System.out.println(ss2);
 	}
 
 }
